f04c2e74753ef2b5f048db8f4480bf53fbefb852
idsudeep <sudeep.numb@gmail.com>
2021-04-17 01:39:49 +0530

    fileCheck

---
+++ q.php
@@ -0,0 +1,37 @@
+<?php
+
+$filenam= rand();
+
+error_reporting(E_ALL);
+
+$pagename = 'hold';
+
+$newFileName = './folder/'.$pagename.".php";
+$newFileContent = '<?php echo "Anything..."; ?>';
+
+
+
+if (file_put_contents($newFileName, $newFileContent) !== false) {
+    echo "File created (" . basename($newFileName) . ")";
+} else {
+    echo "Cannot create file (" . basename($newFileName) . ")";
+}
+
+/*
+array_map('unlink', array_filter(
+    (array) array_merge(glob("folder/*"))));
+
+    file_put_contents('index.php',' <?php echo index; ?>'); 
+
+    unlink($files[0]);
+*/ 
+
+$fileDir = './folder/';
+$fileCount = 0;
+$files = glob($fileDir.'/*');
+if($files){
+    $fileCount = count($files);
+}
+echo 'Total no of file present in Folder is '.$fileCount;
+    
+?>
